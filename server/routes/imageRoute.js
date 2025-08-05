982a191577c71fd9f1a04c0116710d3369c06494
MONSTER-SARAVANAN <saravananstr600@gmail.com>
2025-08-05 18:43:30 +0530

    oNe

--- server/routes/imageRoute.js
+++ server/routes/imageRoute.js
@@ -1,8 +1,8 @@
-import expres from 'express'
+import express from 'express'
 import { generateImage } from '../controllers/imageController.js'
 import userAuth from '../middlewares/auth.js'
 
-const imageRouter = expres.Router()
+const imageRouter = express.Router()
 
 imageRouter.post('/generate-image', userAuth, generateImage)
 
